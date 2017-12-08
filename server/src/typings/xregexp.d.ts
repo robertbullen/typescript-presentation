e3e140716a9e35800758971bac108966e466124d
Robert Bullen <robert@robertbullen.com>
2017-12-08 15:08:51 -0600

    ...try again

--- server/src/typings/xregexp.d.ts
+++ server/src/typings/xregexp.d.ts
@@ -1,8 +1,8 @@
 import * as xregexp from 'xregexp';
 
 declare module 'xregexp' {
-    // The public declaration specifies `subs` as an array, but XRegExp's documentation and experience dictates that it
-    // accepts an object. Whether it is strictly an object or can be either an object or array is 
+    // The public declaration specifies `subs` as an array, but XRegExp's documentation stipulates that it is an object
+    // whose property values are strings or regular expression objects, so this overload is more accurate.
     function build(pattern: string, subs: Record<string, string | RegExp>, flags?: string): RegExp;
 
     // The public declaration returns a boolean, but returning a type assertion is more helpful.
