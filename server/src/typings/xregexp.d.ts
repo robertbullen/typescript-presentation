f6b4b68594a2b352605dd5707e7db679b27e7875
Robert Bullen <robert@robertbullen.com>
2017-12-08 15:06:42 -0600

    improved the xregexp type definition file

--- server/src/typings/xregexp.d.ts
+++ server/src/typings/xregexp.d.ts
@@ -1,6 +1,10 @@
 import * as xregexp from 'xregexp';
 
 declare module 'xregexp' {
-    function build(pattern: string, subs: object, flags?: string): RegExp;
+    // The public declaration specifies `subs` as an array, but XRegExp's documentation and experience dictates that it
+    // accepts an object. Whether it is strictly an object or can be either an object or array is 
+    function build(pattern: string, subs: Record<string, string | RegExp>, flags?: string): RegExp;
+
+    // The public declaration returns a boolean, but returning a type assertion is more helpful.
     function isRegExp(value: any): value is RegExp;
 }
